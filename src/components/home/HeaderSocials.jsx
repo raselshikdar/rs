f21cf8639b33204c003a4b3309a57c7caa63a249
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-06-06 22:23:06 +0600

    Update HeaderSocials.jsx

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -5,27 +5,27 @@ import { SiHashnode } from 'react-icons/si'
 const HeaderSocials = () => {
     return (
         <div className='home__socials'>
-            <a href='https://www.github.com/GregSithole' className='home__social-link' target='_blank' rel='noreferrer'>
+            <a href='https://www.github.com/raselshikdar' className='home__social-link' target='_blank' rel='noreferrer'>
                 <FaGithub />
             </a>
 
-            <a href='https://hashnode.com/@gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
+            <a href='https://hashnode.com/@rasel' className='home__social-link' target='_blank' rel='noreferrer'>
                 <SiHashnode />
             </a>
 
-            <a href='https://dev.to/gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
+            <a href='https://dev.to/raselshikdar' className='home__social-link' target='_blank' rel='noreferrer'>
                 <FaDev />
             </a>
 
-            <a href='https://www.linkedin.com/in/greg-sithole-b6350558/' className='home__social-link' target='_blank' rel='noreferrer'>
+            <a href='https://www.linkedin.com/in/rasel597/' className='home__social-link' target='_blank' rel='noreferrer'>
                 <FaLinkedinIn />
             </a>
 
-            <a href='https://wwww.facebook.com/Greg.Sithole' className='home__social-link' target='_blank' rel='noreferrer'>
+            <a href='https://wwww.facebook.com/raselshiikdar' className='home__social-link' target='_blank' rel='noreferrer'>
                 <FaFacebookF />
             </a>
 
-            <a href='https://www.twitter.com/GregSithole' className='home__social-link' target='__blank' rel='noreferrer'>
+            <a href='https://www.twitter.com/RaselShikdar_' className='home__social-link' target='__blank' rel='noreferrer'>
                 <FaTwitter />
             </a>
         </div>
